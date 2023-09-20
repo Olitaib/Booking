d5fc3cf5e6c09a5bf3ec0f7b8fc7401d7b647bd3
Olitaib <v.kuzmich98@gmail.com>
2023-09-20 20:52:48 +0300

    features:[BP-4] introduce booking

---
+++ app/Models/Booking.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'room_id',
+        'user_id',
+        'started_at',
+        'finished_at',
+        'days',
+        'price',
+    ];
+
+}
