b1caf6dbed81c8dedd7a261be2bc08f291c82f72
Olitaib <v.kuzmich98@gmail.com>
2023-10-02 14:25:29 +0300

    features:[BP-7] include frontend

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,24 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Booking;
+use Illuminate\View\View;
 
 class BookingController extends Controller
 {
 
-    public function index()
+    public function index(): View
     {
-        $bookings = Booking::all();
+        $bookings = Booking::whereBelongsTo(auth()->user())->get();
 
-        return response()->json($bookings);
+        return view('bookings.index', ['bookings' => $bookings]);
     }
 
-    public function show(Request $request)
+    public function show(Request $request): View
     {
         $booking = Booking::find($request['id']);
 
-        return response()->json($booking);
+        return view('bookings.show', ['booking' => $booking]);
     }
 
+    public function store(Request $request)
+    {
+
+        Booking::create([
+            'user_id' => auth()->id(),
+            'room_id' => $request->room_id,
+            'started_at' => $request->started_at,
+            'finished_at' => $request->finished_at,
+            'days' => $days = Carbon::createFromDate($request->finished_at)->diffInDays($request->started_at),
+            'price' => $days * Room::find($request->room_id)->price,
+        ]);
+
+        return redirect('bookings');
+    }
 }
