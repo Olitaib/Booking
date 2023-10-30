485e5bbc6bb689f63a15cae00e6655ceff154929
Olitaib <v.kuzmich98@gmail.com>
2023-10-30 12:26:06 +0300

    features:[BP-9] add filter hotels

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Facility;
 use App\Models\Room;
 use Illuminate\Http\Request;
 use App\Models\Hotel;
@@ -9,11 +10,17 @@ use Illuminate\View\View;
 
 class HotelController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
         $hotels = Hotel::all();
 
-        return view('hotels.index', ['hotels' => $hotels]);
+        if ($request['sort']) {
+            $hotels = $this->filterHotels($request['sort'], $request['filters']);
+        }
+
+        $facilities = Facility::query()->whereHas('hotels')->get();
+
+        return view('hotels.index', ['hotels' => $hotels, 'facilities' => $facilities]);
     }
 
     public function show(Request $request): View
@@ -31,6 +38,33 @@ class HotelController extends Controller
         }
 
         return view('hotels.show', ['hotel' => $hotel, 'rooms' => $rooms]);
+    }
+
+    public function filterHotels(string $sort, ?array $filters)
+    {
+        $hotels = Hotel::query()
+            ->select('h.*', Room::raw('MAX(r.price) as maxPrice'), Room::raw('MIN(r.price) as minPrice'))
+            ->from('hotels as h')
+            ->join('rooms as r', 'r.hotel_id', '=', 'h.id')
+            ->groupBy('h.id');
+
+        if ($filters) {
+            $count = count($filters);
+            $hotels
+                ->join('facility_hotels as fh', 'fh.hotel_id', '=', 'h.id')
+                ->join('facilities as f', 'f.id', '=', 'fh.facility_id')
+                ->whereIn('f.id', $filters)
+                ->groupBy('h.id')
+                ->havingRaw("COUNT(DISTINCT f.id) = $count");
+        }
+
+        $sort = explode('_', $sort);
+
+        return $hotels
+            ->distinct()
+            ->orderBy($sort['0'], $sort['1'])
+            ->get();
+
     }
 
 }
